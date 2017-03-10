6b95cac29a60f350e457b8a2995d564b5c07328e
Harrison Harnisch <hharnisc@gmail.com>
2017-03-10 15:05:52 -0600

    start a micro server with the node dashboard

---
+++ layout.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = [
+  [
+    {
+      views: [
+        {
+          name: "cpu",
+          limit: 30
+        },
+        {
+          name: "memory",
+          position: {
+            size: 15
+          }
+        }
+      ]
+    }
+  ]
+];
